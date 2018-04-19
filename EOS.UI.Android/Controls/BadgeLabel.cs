093b614891e79a5a6a15234f581692d683c1bba9
Vlad Zagubinoga <vzagubinoga@ego-cms.com>
2018-04-19 11:56:49 +0300

    EU-13 Implemented IEOSThemeControl

--- EOS.UI.Android/Controls/BadgeLabel.cs
+++ EOS.UI.Android/Controls/BadgeLabel.cs
@@ -8,17 +8,13 @@ using Android.Widget;
 using EOS.UI.Shared.Themes.Helpers;
 using EOS.UI.Shared.Themes.Interfaces;
 using UIFrameworks.Android.Themes;
+using UIFrameworks.Shared.Themes.Helpers;
 using UIFrameworks.Shared.Themes.Interfaces;
 
 namespace EOS.UI.Android.Controls
 {
     public class BadgeLabel: TextView, IEOSThemeControl
     {
-        #region fields
-
-
-        #endregion
-
         #region constructors
 
         public BadgeLabel(Context context) : base(context)
@@ -47,32 +43,37 @@ namespace EOS.UI.Android.Controls
 
         public void SetCustomBackgroundColor(Color color)
         {
+            IsEOSCustomizationIgnored = false;
             (Background as GradientDrawable).SetColor(color);
         }
 
-        public void SetCustomFont(string fontPath)
+        public void SetCustomFont(Typeface fontTypeFace)
         {
-            var font = Typeface.CreateFromAsset(Context.Assets, fontPath);
-            SetTypeface(font, TypefaceStyle.Normal);
+            IsEOSCustomizationIgnored = false;
+            SetTypeface(fontTypeFace, TypefaceStyle.Normal);
         }
 
         public void SetCustomLetterSpacing(float spacing)
         {
+            IsEOSCustomizationIgnored = false;
             LetterSpacing = spacing;
         }
 
         public void SetCustomTextColor(Color color)
         {
+            IsEOSCustomizationIgnored = false;
             SetTextColor(color);
         }
 
         public void SetCustomTextSize(float size)
         {
+            IsEOSCustomizationIgnored = false;
             TextSize = size;
         }
 
         public void SetCornerRadius(float radius)
         {
+            IsEOSCustomizationIgnored = false;
             (Background as GradientDrawable).SetCornerRadius(radius);
         }
 
@@ -112,17 +113,26 @@ namespace EOS.UI.Android.Controls
 
         public void UpdateAppearance()
         {
-            
+            if(IsEOSCustomizationIgnored)
+            {
+                (Background as GradientDrawable).SetColor(GetThemeProvider().GetEOSProperty<Color>(this, EOSConstants.BackgroundColor));
+                SetTypeface(Typeface.CreateFromAsset(Context.Assets, GetThemeProvider().GetEOSProperty<string>(this, EOSConstants.Font)), TypefaceStyle.Normal);
+                LetterSpacing = GetThemeProvider().GetEOSProperty<float>(this, EOSConstants.LetterSpacing);
+                SetTextColor(GetThemeProvider().GetEOSProperty<Color>(this, EOSConstants.TextColor));
+                TextSize = GetThemeProvider().GetEOSProperty<float>(this, EOSConstants.TextSize);
+                (Background as GradientDrawable).SetCornerRadius(GetThemeProvider().GetEOSProperty<float>(this, EOSConstants.CornerRadius));
+            }
         }
 
         public void ResetCustomization()
         {
-            
+            IsEOSCustomizationIgnored = true;
+            UpdateAppearance();
         }
 
         public IEOSStyle GetCurrentEOSStyle()
         {
-            throw new NotImplementedException();
+            return null;
         }
 
         public IEOSStyle SetEOSStyle(EOSStyleEnumeration style)
