7652ed8ed1ed7054d21596520de364d3efa82c34
Igor-mac <igor@Igor.local>
2018-04-24 17:13:11 +0300

    Changed constants for iOS according to droid

--- EOS.UI.iOS.Sandbox/Helpers/Constants.cs
+++ EOS.UI.iOS.Sandbox/Helpers/Constants.cs
@@ -8,24 +8,24 @@ namespace EOS.UI.iOS.Sandbox.Helpers
 {
     public static class Constants
     {
-        public static UIColor BackgroundColor = UIColor.FromRGB(65, 65, 65);
+        public static UIColor BackgroundColor = UIColor.FromRGB(224, 224, 224);
 
         public static Dictionary<string, UIColor> Colors = new Dictionary<string, UIColor>()
         {
+            {"Black", UIColor.Black},
             {"White", UIColor.White},
-            {"Red", UIColor.Red},
+            {"Gray", UIColor.Gray},
             {"Green", UIColor.Green},
             {"Blue", UIColor.Blue},
-            {"Gray", UIColor.Gray},
+            {"Red", UIColor.Red},
             {"Yellow", UIColor.Yellow},
-            {"Orange", UIColor.Orange},
-            {"Black", UIColor.Black}
+            {"Brown", UIColor.Brown},
         };
-        public static List<int> CornerRadiusValues = new List<int>() {0, 1, 2, 3, 4, 5, 7, 8, 9, 10 };
+        public static List<int> CornerRadiusValues;
 
-        public static List<int> FontSizeValues = new List<int>() { 17, 19, 24, 32, 40 };
+        public static List<int> FontSizeValues;
 
-        public static List<int> LetterSpacingValues = new List<int>() { 1, 2, 3, 4, 5, 6, 7 };
+        public static List<int> LetterSpacingValues;
 
         public static Dictionary<string, EOSThemeEnumeration> Themes = new Dictionary<string, EOSThemeEnumeration>()
             {
@@ -49,6 +49,9 @@ namespace EOS.UI.iOS.Sandbox.Helpers
             }
 
             Fonts = Fonts.OrderBy(f => f.Name).ToList();
+            FontSizeValues = Enumerable.Range(10, 31).Where(i=>i%2==0).ToList();
+            CornerRadiusValues = Enumerable.Range(10, 61).Where(i => (i-10) % 4 == 0).ToList();
+            LetterSpacingValues = Enumerable.Range(1, 10).ToList();
         }
     }
 }
