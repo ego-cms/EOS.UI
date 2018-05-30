125bd9f793a9b814ba5ebb1208fb5d0d32cf441d
Pavel Chehov <pavel.chehov.personal@gmail.com>
2018-05-30 10:48:32 +0300

    fixed themes

--- EOS.UI.Shared.Themes/Themes/DarkEOSTheme.cs
+++ EOS.UI.Shared.Themes/Themes/DarkEOSTheme.cs
@@ -31,8 +31,8 @@ namespace EOS.UI.Shared.Themes.Themes
         private const string neutralColor2 = "#F6F6F6";
         private const string neutralColor3 = "#EAEAEA";
         private const string neutralColor4 = "#C0BFC0";
-        private const string neutralColor5 = "#C0BFC0";
-        private const string neutralColor6 = "#2E2D2E";
+        private const string neutralColor5 = "#777677";
+        private const string neutralColor6 = "#343334";
 
         public Dictionary<string, object> ThemeValues => new Dictionary<string, object>()
         {
