254c2f1c7a394fd8602575815af0a1e6c39770b4
Pavel Chehov <pavel.chehov.personal@gmail.com>
2018-05-31 10:16:54 +0300

    added fillcolor to the circle

--- EOS.UI.iOS.Sandbox/Storyboards/CircleProgressView.designer.cs
+++ EOS.UI.iOS.Sandbox/Storyboards/CircleProgressView.designer.cs
@@ -1,88 +1,95 @@
-﻿// WARNING
+// WARNING
 //
-// This file has been generated automatically by Visual Studio from the outlets and
-// actions declared in your storyboard file.
-// Manual changes to this file will not be maintained.
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
 //
 using Foundation;
-using System;
 using System.CodeDom.Compiler;
 
 namespace EOS.UI.iOS.Sandbox
 {
-    [Register ("CircleProgressView")]
-    partial class CircleProgressView
-    {
-        [Outlet]
-        UIKit.UIView containerView { get; set; }
+	[Register ("CircleProgressView")]
+	partial class CircleProgressView
+	{
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		EOS.UI.iOS.Sandbox.CustomDropDown alternativeColorDropDown { get; set; }
 
-        [Outlet]
-        UIKit.UIButton resetButton { get; set; }
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		EOS.UI.iOS.Sandbox.CustomDropDown colorDropDown { get; set; }
 
-        [Outlet]
-        UIKit.UISwitch showProgressSwitch { get; set; }
+		[Outlet]
+		UIKit.UIView containerView { get; set; }
 
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        EOS.UI.iOS.Sandbox.CustomDropDown alternativeColorDropDown { get; set; }
+		[Outlet]
+		EOS.UI.iOS.Sandbox.CustomDropDown fillColorDropDown { get; set; }
 
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        EOS.UI.iOS.Sandbox.CustomDropDown colorDropDown { get; set; }
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		EOS.UI.iOS.Sandbox.CustomDropDown fontDropDown { get; set; }
 
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        EOS.UI.iOS.Sandbox.CustomDropDown fontDropDown { get; set; }
+		[Outlet]
+		UIKit.UIButton resetButton { get; set; }
 
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        EOS.UI.iOS.Sandbox.CustomDropDown textSizeDropDown { get; set; }
+		[Outlet]
+		UIKit.UISwitch showProgressSwitch { get; set; }
 
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        EOS.UI.iOS.Sandbox.CustomDropDown themeDropDown { get; set; }
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		EOS.UI.iOS.Sandbox.CustomDropDown textSizeDropDown { get; set; }
 
-        void ReleaseDesignerOutlets ()
-        {
-            if (alternativeColorDropDown != null) {
-                alternativeColorDropDown.Dispose ();
-                alternativeColorDropDown = null;
-            }
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		EOS.UI.iOS.Sandbox.CustomDropDown themeDropDown { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (containerView != null) {
+				containerView.Dispose ();
+				containerView = null;
+			}
 
-            if (colorDropDown != null) {
-                colorDropDown.Dispose ();
-                colorDropDown = null;
-            }
+			if (resetButton != null) {
+				resetButton.Dispose ();
+				resetButton = null;
+			}
 
-            if (containerView != null) {
-                containerView.Dispose ();
-                containerView = null;
-            }
+			if (showProgressSwitch != null) {
+				showProgressSwitch.Dispose ();
+				showProgressSwitch = null;
+			}
 
-            if (fontDropDown != null) {
-                fontDropDown.Dispose ();
-                fontDropDown = null;
-            }
+			if (alternativeColorDropDown != null) {
+				alternativeColorDropDown.Dispose ();
+				alternativeColorDropDown = null;
+			}
 
-            if (resetButton != null) {
-                resetButton.Dispose ();
-                resetButton = null;
-            }
+			if (colorDropDown != null) {
+				colorDropDown.Dispose ();
+				colorDropDown = null;
+			}
 
-            if (showProgressSwitch != null) {
-                showProgressSwitch.Dispose ();
-                showProgressSwitch = null;
-            }
+			if (fontDropDown != null) {
+				fontDropDown.Dispose ();
+				fontDropDown = null;
+			}
 
-            if (textSizeDropDown != null) {
-                textSizeDropDown.Dispose ();
-                textSizeDropDown = null;
-            }
+			if (textSizeDropDown != null) {
+				textSizeDropDown.Dispose ();
+				textSizeDropDown = null;
+			}
 
-            if (themeDropDown != null) {
-                themeDropDown.Dispose ();
-                themeDropDown = null;
-            }
-        }
-    }
+			if (themeDropDown != null) {
+				themeDropDown.Dispose ();
+				themeDropDown = null;
+			}
+
+			if (fillColorDropDown != null) {
+				fillColorDropDown.Dispose ();
+				fillColorDropDown = null;
+			}
+		}
+	}
 }
