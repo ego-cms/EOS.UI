1c7674a59a3138a3d9c399d85d08644cfe635869
Pavel Chehov <pavel.chehov.personal@gmail.com>
2018-06-14 10:12:05 +0300

    added WorkTimeCalendarCollection

--- EOS.UI.iOS.Sandbox/Storyboards/WorkTimeView.designer.cs
+++ EOS.UI.iOS.Sandbox/Storyboards/WorkTimeView.designer.cs
@@ -1,130 +1,145 @@
 // WARNING
 //
-// This file has been generated automatically by Visual Studio to store outlets and
-// actions made in the UI designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
+using EOS.UI.iOS.Components;
 using Foundation;
+using System;
 using System.CodeDom.Compiler;
 
 namespace EOS.UI.iOS.Sandbox
 {
-	[Register ("WorkTimeView")]
-	partial class WorkTimeView
-	{
-		[Outlet]
-		EOS.UI.iOS.Sandbox.EOSSandboxDropDown currentDayBackgroundColorDropDown { get; set; }
+    [Register ("WorkTimeView")]
+    partial class WorkTimeView
+    {
+        [Outlet]
+        EOS.UI.iOS.Sandbox.EOSSandboxDropDown currentDayBackgroundColorDropDown { get; set; }
 
-		[Outlet]
-		EOS.UI.iOS.Sandbox.EOSSandboxDropDown currentDayDevidersColor { get; set; }
 
-		[Outlet]
-		EOS.UI.iOS.Sandbox.EOSSandboxDropDown currentDayTextColorDropDown { get; set; }
+        [Outlet]
+        EOS.UI.iOS.Sandbox.EOSSandboxDropDown currentDayDevidersColor { get; set; }
 
-		[Outlet]
-		EOS.UI.iOS.Sandbox.EOSSandboxDropDown dayEvenBackgroundColor { get; set; }
 
-		[Outlet]
-		EOS.UI.iOS.Sandbox.EOSSandboxDropDown dayFontDropDown { get; set; }
+        [Outlet]
+        EOS.UI.iOS.Sandbox.EOSSandboxDropDown currentDayTextColorDropDown { get; set; }
 
-		[Outlet]
-		EOS.UI.iOS.Sandbox.EOSSandboxDropDown dayTextColorDropDown { get; set; }
 
-		[Outlet]
-		EOS.UI.iOS.Sandbox.EOSSandboxDropDown dayTextSizeDropDown { get; set; }
+        [Outlet]
+        EOS.UI.iOS.Sandbox.EOSSandboxDropDown dayEvenBackgroundColor { get; set; }
 
-		[Outlet]
-		EOS.UI.iOS.Sandbox.EOSSandboxDropDown devidersColor { get; set; }
 
-		[Outlet]
-		UIKit.UIButton resetButton { get; set; }
+        [Outlet]
+        EOS.UI.iOS.Sandbox.EOSSandboxDropDown dayFontDropDown { get; set; }
 
-		[Outlet]
-		EOS.UI.iOS.Sandbox.EOSSandboxDropDown themesDropDown { get; set; }
 
-		[Outlet]
-		EOS.UI.iOS.Sandbox.EOSSandboxDropDown titleFontDropDown { get; set; }
+        [Outlet]
+        EOS.UI.iOS.Sandbox.EOSSandboxDropDown dayTextColorDropDown { get; set; }
 
-		[Outlet]
-		EOS.UI.iOS.Sandbox.EOSSandboxDropDown titleSizeDropDown { get; set; }
 
-		[Outlet]
-		EOS.UI.iOS.Sandbox.EOSSandboxDropDown weekStartDropdown { get; set; }
+        [Outlet]
+        EOS.UI.iOS.Sandbox.EOSSandboxDropDown dayTextSizeDropDown { get; set; }
 
-		[Outlet]
-		UIKit.UICollectionView workTimeCollection { get; set; }
-		
-		void ReleaseDesignerOutlets ()
-		{
-			if (currentDayBackgroundColorDropDown != null) {
-				currentDayBackgroundColorDropDown.Dispose ();
-				currentDayBackgroundColorDropDown = null;
-			}
 
-			if (currentDayTextColorDropDown != null) {
-				currentDayTextColorDropDown.Dispose ();
-				currentDayTextColorDropDown = null;
-			}
+        [Outlet]
+        EOS.UI.iOS.Sandbox.EOSSandboxDropDown devidersColor { get; set; }
 
-			if (dayEvenBackgroundColor != null) {
-				dayEvenBackgroundColor.Dispose ();
-				dayEvenBackgroundColor = null;
-			}
 
-			if (dayFontDropDown != null) {
-				dayFontDropDown.Dispose ();
-				dayFontDropDown = null;
-			}
+        [Outlet]
+        UIKit.UIButton resetButton { get; set; }
 
-			if (dayTextColorDropDown != null) {
-				dayTextColorDropDown.Dispose ();
-				dayTextColorDropDown = null;
-			}
 
-			if (dayTextSizeDropDown != null) {
-				dayTextSizeDropDown.Dispose ();
-				dayTextSizeDropDown = null;
-			}
+        [Outlet]
+        EOS.UI.iOS.Sandbox.EOSSandboxDropDown themesDropDown { get; set; }
 
-			if (resetButton != null) {
-				resetButton.Dispose ();
-				resetButton = null;
-			}
 
-			if (themesDropDown != null) {
-				themesDropDown.Dispose ();
-				themesDropDown = null;
-			}
+        [Outlet]
+        EOS.UI.iOS.Sandbox.EOSSandboxDropDown titleFontDropDown { get; set; }
 
-			if (titleFontDropDown != null) {
-				titleFontDropDown.Dispose ();
-				titleFontDropDown = null;
-			}
 
-			if (titleSizeDropDown != null) {
-				titleSizeDropDown.Dispose ();
-				titleSizeDropDown = null;
-			}
+        [Outlet]
+        EOS.UI.iOS.Sandbox.EOSSandboxDropDown titleSizeDropDown { get; set; }
 
-			if (weekStartDropdown != null) {
-				weekStartDropdown.Dispose ();
-				weekStartDropdown = null;
-			}
 
-			if (workTimeCollection != null) {
-				workTimeCollection.Dispose ();
-				workTimeCollection = null;
-			}
+        [Outlet]
+        EOS.UI.iOS.Sandbox.EOSSandboxDropDown weekStartDropdown { get; set; }
 
-			if (devidersColor != null) {
-				devidersColor.Dispose ();
-				devidersColor = null;
-			}
 
-			if (currentDayDevidersColor != null) {
-				currentDayDevidersColor.Dispose ();
-				currentDayDevidersColor = null;
-			}
-		}
-	}
+        [Outlet]
+        WorkTimeCalendarCollection workTimeCollection { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (currentDayBackgroundColorDropDown != null) {
+                currentDayBackgroundColorDropDown.Dispose ();
+                currentDayBackgroundColorDropDown = null;
+            }
+
+            if (currentDayDevidersColor != null) {
+                currentDayDevidersColor.Dispose ();
+                currentDayDevidersColor = null;
+            }
+
+            if (currentDayTextColorDropDown != null) {
+                currentDayTextColorDropDown.Dispose ();
+                currentDayTextColorDropDown = null;
+            }
+
+            if (dayEvenBackgroundColor != null) {
+                dayEvenBackgroundColor.Dispose ();
+                dayEvenBackgroundColor = null;
+            }
+
+            if (dayFontDropDown != null) {
+                dayFontDropDown.Dispose ();
+                dayFontDropDown = null;
+            }
+
+            if (dayTextColorDropDown != null) {
+                dayTextColorDropDown.Dispose ();
+                dayTextColorDropDown = null;
+            }
+
+            if (dayTextSizeDropDown != null) {
+                dayTextSizeDropDown.Dispose ();
+                dayTextSizeDropDown = null;
+            }
+
+            if (devidersColor != null) {
+                devidersColor.Dispose ();
+                devidersColor = null;
+            }
+
+            if (resetButton != null) {
+                resetButton.Dispose ();
+                resetButton = null;
+            }
+
+            if (themesDropDown != null) {
+                themesDropDown.Dispose ();
+                themesDropDown = null;
+            }
+
+            if (titleFontDropDown != null) {
+                titleFontDropDown.Dispose ();
+                titleFontDropDown = null;
+            }
+
+            if (titleSizeDropDown != null) {
+                titleSizeDropDown.Dispose ();
+                titleSizeDropDown = null;
+            }
+
+            if (weekStartDropdown != null) {
+                weekStartDropdown.Dispose ();
+                weekStartDropdown = null;
+            }
+
+            if (workTimeCollection != null) {
+                workTimeCollection.Dispose ();
+                workTimeCollection = null;
+            }
+        }
+    }
 }
---
+++ EOS.UI.iOS/Components/WorkTimeCalendarCollection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using CoreGraphics;
+using EOS.UI.iOS.CollectionViewSources;
+using Foundation;
+using UIKit;
+
+namespace EOS.UI.iOS.Components
+{
+    [Register("WorkTimeCalendarCollection")]
+    public class WorkTimeCalendarCollection : UICollectionView
+    {
+        public WorkTimeCalendarCollection(NSCoder coder) : base(coder)
+        {
+            Initalize();
+        }
+
+        public WorkTimeCalendarCollection(CGRect frame, UICollectionViewLayout layout) : base(frame, layout)
+        {
+            Initalize();
+        }
+
+        protected WorkTimeCalendarCollection(NSObjectFlag t) : base(t)
+        {
+            Initalize();
+        }
+
+        protected internal WorkTimeCalendarCollection(IntPtr handle) : base(handle)
+        {
+            Initalize();
+        }
+        
+        private void Initalize()
+        {
+            Source = new WorkTimeCalendarCollectionSource(this);
+        }
+    }
+}
