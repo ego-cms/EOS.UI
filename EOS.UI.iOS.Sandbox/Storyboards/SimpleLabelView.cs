5e0b3f5283a67ea913dd108fd9291d7e2e25fa91
Pavel Chehov <pavel.chehov.personal@gmail.com>
2018-07-03 15:44:34 +0300

    rename labels

--- EOS.UI.iOS.Sandbox/Storyboards/SimpleLabelView.cs
+++ EOS.UI.iOS.Sandbox/Storyboards/SimpleLabelView.cs
@@ -31,7 +31,7 @@ namespace EOS.UI.iOS.Sandbox
 
             _simpleLabel = new SimpleLabel
             {
-                Text = ControlNames.SimpleLabel
+                Text = "Label"
             };
 
             _dropDowns = new List<EOSSandboxDropDown>()
