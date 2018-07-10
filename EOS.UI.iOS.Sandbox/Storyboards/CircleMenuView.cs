1187a049e14be046c228e9e66ffe3ebf88401a84
Pavel Chehov <pavel.chehov.personal@gmail.com>
2018-07-10 15:53:07 +0300

    added circlemenu storyboard

---
+++ EOS.UI.iOS.Sandbox/Storyboards/CircleMenuView.cs
@@ -0,0 +1,16 @@
+using EOS.UI.iOS.Sandbox.Storyboards;
+using Foundation;
+using System;
+using UIKit;
+
+namespace EOS.UI.iOS.Sandbox
+{
+    public partial class CircleMenuView : BaseViewController
+    {
+        public const string Identifier = "CircleMenuView";
+        
+        public CircleMenuView (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
