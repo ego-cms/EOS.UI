38ae887dc5f29f76c53d74f4d63bd460ac9665b1
Pavel Chehov <pavel.chehov.personal@gmail.com>
2018-07-16 14:09:58 +0300

    added show/hide buttons animations

--- EOS.UI.iOS/Components/CircleMenu/CircleMenu.cs
+++ EOS.UI.iOS/Components/CircleMenu/CircleMenu.cs
@@ -15,17 +15,18 @@ namespace EOS.UI.iOS.Components
     {
         private const int _minimumCountOfElements = 3;
         private const int _maximumCountOfElements = 9;
+        private const int _visibleCountOfElements = 5;
         private const int _menuSize = 300;
-        
+
         private const int _radius = 96;
         private readonly nfloat _180degrees = 3.14159f;
         private readonly nfloat _36degrees = 0.628319f;
-        
+
         private readonly UIView _rootView;
         private readonly UIView _shadowView;
         private readonly UISwipeGestureRecognizer _leftSwipe;
         private readonly UISwipeGestureRecognizer _rightSwipe;
-        
+
         private readonly CASpringAnimation _menuOpenAnimation;
         private readonly CASpringAnimation _menuCloseAnimation;
         //view for circle of menu buttons
@@ -105,9 +106,16 @@ namespace EOS.UI.iOS.Components
             _menuOpenAnimation.To = new NSNumber(-_180degrees);
             _menuOpenAnimation.RepeatCount = 1;
             _menuOpenAnimation.RemovedOnCompletion = true;
+            _menuOpenAnimation.AnimationStarted += (sender, e) => _mainButton.UserInteractionEnabled = false;
             _menuOpenAnimation.AnimationStopped += (s, e) =>
             {
                 _menuButtonsView.Transform = CGAffineTransform.MakeRotation(-_180degrees);
+                _menuButtons.ForEach(b =>
+                {
+                    b.Hidden = false;
+                    b.Alpha = 1.0f;
+                });
+                _mainButton.UserInteractionEnabled = true;
             };
 
             _menuCloseAnimation = new CASpringAnimation();
@@ -117,9 +125,16 @@ namespace EOS.UI.iOS.Components
             _menuCloseAnimation.To = new NSNumber(0f);
             _menuCloseAnimation.RepeatCount = 1;
             _menuCloseAnimation.RemovedOnCompletion = true;
+            _menuCloseAnimation.AnimationStarted += (sender, e) => _mainButton.UserInteractionEnabled = false;
             _menuCloseAnimation.AnimationStopped += (s, e) =>
             {
                 _menuButtonsView.Transform = CGAffineTransform.MakeRotation(0);
+                _menuButtons.ForEach(b =>
+                {
+                    b.Hidden = true;
+                    b.Alpha = 0.0f;
+                });
+                _mainButton.UserInteractionEnabled = true;
             };
 
             AddSubview(_mainButton);
@@ -131,7 +146,7 @@ namespace EOS.UI.iOS.Components
             CreateMenuButtons();
         }
 
-        
+
 
         void CreateMenuButtons()
         {
@@ -139,7 +154,7 @@ namespace EOS.UI.iOS.Components
             double endAngle = 6.28 + startAngle;
             double diff = (endAngle - startAngle) / _maximumCountOfElements;
 
-            for (int i = 0; i < _source.Count; ++i)
+            foreach (var model in _source)
             {
                 var x = Math.Cos(startAngle) * _radius + _menuButtonsView.Bounds.GetCenterX() - CircleMenuMainButton.Size / 2;
                 var y = Math.Sin(startAngle) * _radius + _menuButtonsView.Bounds.GetCenterY() - CircleMenuMainButton.Size / 2;
@@ -149,14 +164,14 @@ namespace EOS.UI.iOS.Components
                     TintColor = UIColor.Black,
                     BackgroundColor = UIColor.White,
                     Frame = new CGRect((nfloat)x, (nfloat)y, CircleMenuMainButton.Size, CircleMenuMainButton.Size),
-                    //Hidden = true,
-                    //Alpha = 0.0f
+                    Hidden = true,
+                    Alpha = 0.0f
                 };
                 //menuButton.SetImage(model.ImageSource, UIControlState.Normal);
                 ///TODO need to remove
-                menuButton.SetTitle(_source[i].Id.ToString(), UIControlState.Normal);
+                menuButton.SetTitle(model.Id.ToString(), UIControlState.Normal);
                 menuButton.SetTitleColor(UIColor.Black, UIControlState.Normal);
-                menuButton.TouchUpInside += (sender, e) => _source[i].Clicked?.Invoke(menuButton, EventArgs.Empty);
+                menuButton.TouchUpInside += (sender, e) => model.Clicked?.Invoke(menuButton, EventArgs.Empty);
 
                 _menuButtonsView.InsertSubview(menuButton, 0);
                 _menuButtons.Add(menuButton);
@@ -168,41 +183,39 @@ namespace EOS.UI.iOS.Components
         {
             _menuButtonsView.Layer.AddAnimation(_menuOpenAnimation, null);
 
-            //UIView.Animate(5, 0, UIViewAnimationOptions.TransitionCrossDissolve, () =>
-            //{
-            //    for (int i = 0; i < _minimumCountOfElements; ++i)
-            //    {
-            //        _menuButtons[i].Hidden = false;
-            //    }
-            //    _menuButtonsView.Transform = CGAffineTransform.MakeRotation(-_180degrees);
-            //}, () => 
-            //{
-            //    _menuButtons.ForEach(b => b.Hidden = false);
-            //});
+            var count = _source.Count >= _visibleCountOfElements ? _visibleCountOfElements : _source.Count;
+            UIView.Animate(0.8, () =>
+            {
+                for (int i = 0; i < count; ++i)
+                {
+                    _menuButtons[i].Hidden = false;
+                    _menuButtons[i].Alpha = 1;
+                }
+            });
         }
 
         void HideMenuButtons()
         {
+            if (_source.Count >= _visibleCountOfElements)
+            {
+                for (int i = _visibleCountOfElements - 1; i < _source.Count; ++i)
+                {
+                    _menuButtons[i].Hidden = true;
+                    _menuButtons[i].Alpha = 0f;
+                }
+            }
+
+            var count = _source.Count >= _visibleCountOfElements ? _visibleCountOfElements : _source.Count;
+            UIView.Animate(0.2, () =>
+            {
+                for (int i = 0; i < count; ++i)
+                {
+                    _menuButtons[i].Alpha = 0f;
+                }
+            });
             _menuButtonsView.Layer.AddAnimation(_menuCloseAnimation, null);
-
-            //UIView.Animate(0.5, () =>
-            //{
-            //    if (_source.Count > _minimumCountOfElements + 1)
-            //    {
-            //        for (int i = _minimumCountOfElements + 1; i < _source.Count; ++i)
-            //        {
-            //            _menuButtons[i].Hidden = true;
-            //        }
-            //    }
-
-            //    _menuButtonsView.Transform = CGAffineTransform.MakeRotation(0);
-            //}, () => 
-            //{
-            //    _menuButtons.ForEach(b => b.Hidden = true);
-            //});
         }
 
-        
         void OnMainButtonClicked(object sender, EventArgs e)
         {
             if (_mainButton.IsOpen)
