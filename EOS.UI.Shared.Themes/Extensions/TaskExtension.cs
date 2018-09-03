e7ffd05b19cd047084859c34a6d0358eac14e2fa
Pavel Chehov <pavel.chehov.personal@gmail.com>
2018-09-03 12:16:42 +0300

    added comments

--- EOS.UI.Shared.Themes/Extensions/TaskExtension.cs
+++ EOS.UI.Shared.Themes/Extensions/TaskExtension.cs
@@ -5,11 +5,23 @@ namespace EOS.UI.Shared.Themes.Extensions
 {
     public static class TaskExtension
     {
+        /// <summary>
+        /// Animates the following animation with task wrapping
+        /// </summary>
+        /// <returns>Task with wrapped animation</returns>
+        /// <param name="animateNextTask">Following task</param>
+        /// <param name="action">Action.</param>
         public static Task AnimateNext(this Task animateNextTask, Action<Task> action)
         {
             return animateNextTask.ContinueWith(action, TaskScheduler.FromCurrentSynchronizationContext());
         }
 
+        /// <summary>
+        /// Animates the following animation with task wrapping
+        /// </summary>
+        /// <returns>Task with wrapped animation</returns>
+        /// <param name="animateNextTask">Following task</param>
+        /// <param name="action">Action.</param>
         public static Task<TResult> AnimateNext<TResult>(this Task animateNextTask, Func<Task, TResult> action)
         {
             return animateNextTask.ContinueWith(action, TaskScheduler.FromCurrentSynchronizationContext());
