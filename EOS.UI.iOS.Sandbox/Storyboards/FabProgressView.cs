438fa336854a864611b5bd674923ecbf7ca06d74
Pavel Chehov <pavel.chehov.personal@gmail.com>
2018-09-05 12:40:59 +0300

    EU-582 fixed FabProgress behaviour

--- EOS.UI.iOS.Sandbox/Storyboards/FabProgressView.cs
+++ EOS.UI.iOS.Sandbox/Storyboards/FabProgressView.cs
@@ -46,8 +46,6 @@ namespace EOS.UI.iOS.Sandbox
 
             _fab.TouchUpInside += async (sender, e) =>
             {
-                if (_fab.InProgress)
-                    return;
                 ToggleAllControlsEnabled(false, _dropDowns, resetButton, enableSwitch);
                 _fab.StartProgressAnimation();
                 await Task.Delay(5000);
