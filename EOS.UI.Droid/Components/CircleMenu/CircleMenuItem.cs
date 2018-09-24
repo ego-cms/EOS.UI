99b6e48c026175f15457e632c3b68795e4f43446
Vlad <vladalogin@ukr.net>
2018-09-24 16:04:56 +0300

    EU-669 Updayed start hint logic

--- EOS.UI.Droid/Components/CircleMenu/CircleMenuItem.cs
+++ EOS.UI.Droid/Components/CircleMenu/CircleMenuItem.cs
@@ -48,7 +48,7 @@ namespace EOS.UI.Droid.Components
 
         public int CircleMenuModelId { get; set; } = -1;
 
-        public bool HasSubMenus { get; set; }
+        public bool HasSubMenus => _hasChildren;
 
         public override bool Enabled
         {
