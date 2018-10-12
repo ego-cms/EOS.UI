e3b4ea639c5ea0c6cffbf758cc696a48a7e6791b
Pavel Chehov <pavel.chehov.personal@gmail.com>
2018-10-12 18:03:08 +0300

    fixed WorkTimeCalendar constructors

--- EOS.UI.iOS/Components/WorkTimeCalendarCollection.cs
+++ EOS.UI.iOS/Components/WorkTimeCalendarCollection.cs
@@ -9,8 +9,19 @@ namespace EOS.UI.iOS.Components
     [Register("WorkTimeCalendarCollection")]
     public class WorkTimeCalendarCollection : UICollectionView
     {
+        public WorkTimeCalendarCollection(): this(CGRect.Empty, new WorkTimeCalendarFlowLayout())
+        {
+
+        }
+
+        public WorkTimeCalendarCollection(CGRect frame) : this(frame, new WorkTimeCalendarFlowLayout())
+        {
+
+        }
+
         public WorkTimeCalendarCollection(NSCoder coder) : base(coder)
         {
+            CollectionViewLayout = new WorkTimeCalendarFlowLayout();
             Initalize();
         }
 
@@ -21,17 +32,20 @@ namespace EOS.UI.iOS.Components
 
         protected WorkTimeCalendarCollection(NSObjectFlag t) : base(t)
         {
+            CollectionViewLayout = new WorkTimeCalendarFlowLayout();
             Initalize();
         }
 
         protected internal WorkTimeCalendarCollection(IntPtr handle) : base(handle)
         {
+            CollectionViewLayout = new WorkTimeCalendarFlowLayout();
             Initalize();
         }
         
         private void Initalize()
         {
             Source = new WorkTimeCalendarCollectionSource(this);
+            BackgroundColor = UIColor.Clear;
         }
     }
 }
